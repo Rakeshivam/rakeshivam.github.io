e9997c66b28368c531cea1d5d9ad40487551d075
Rakesh Kushwaha <rakeshkk589@gmail.com>
2026-07-14 09:05:39 +0530

    app install banner

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'rakesh-portfolio-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/img/RK.jpg',
+  '/assets/img/favicon/manifest.json',
+  '/assets/img/favicon/android-icon-192x192.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => {
+        // Fallback for offline if requested document is html
+        if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/index.html');
+        }
+      });
+    })
+  );
+});
